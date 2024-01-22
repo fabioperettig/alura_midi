16461d38e69da73535647a1d822972639dd2fb6b
Fabio Peretti Guimarães <fabioperetti_pp@outlook.com>
2024-01-22 18:33:45 -0300

    up

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 console.log('Hello Alura Midi')
 
-function playSoundPom() {
-    document.querySelector('#som_tecla_pom').play();
-}
+function playSound(idSound) {
+
+    document.querySelector(idSound).play();
+};
 
 const padList = document.querySelectorAll('.tecla');
 
+var counter = 0;
+
+while (counter < padList.length) {
+
+    const pad = padList[counter];
+    const instrument = pad.classList[1];
+
+    const idAudio = `#som_${instrument}`;
+
+    padList[counter].onclick = function () {
+        playSound(idAudio);
+    }
+    counter ++;
+};
